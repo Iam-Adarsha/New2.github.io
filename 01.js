780de42f0fe4f8062c973ac9e723e7dce46cf7f9
Iam-Adarsha <161053853+Iam-Adarsha@users.noreply.github.com>
2024-02-23 23:53:20 +0530

    commit

---
+++ 01.js
@@ -0,0 +1,13 @@
+const portfolioItems = [
+	{
+		title: 'Website 1',
+		description: 'A website I built for a client using HTML, CSS, and JavaScript.',
+		image: 'https://example.com/images/website1.jpg'
+	},
+	{
+		title: 'Website 2',
+		description: 'Another website I built for a different client using HTML, CSS, and JavaScript.',
+		image: 'https://example.com/images/website2.jpg'
+	},
+	// Add more portfolio items here
+];
